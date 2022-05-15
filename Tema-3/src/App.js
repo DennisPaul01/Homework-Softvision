542d8f83a77358a859ba4c9cf8b067bd8368e5c9
DennisPaul01 <denys_paul08@yahoo.com>
2022-05-15 14:05:43 +0300

    update route

--- Tema-3/src/App.js
+++ Tema-3/src/App.js
@@ -1,3 +1,5 @@
+import { Route, Switch } from "react";
+
 import Header from "./components/Header";
 import Exercitiu1 from "./components/Exercitii/Exercitiu1";
 import Exercitiu2 from "./components/Exercitii/Exercitiu2";
@@ -6,15 +8,19 @@ import Exercitiu4 from "./components/Exercitii/Exercitiu4";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Exercitiu1 />
-      <br />
-      <Exercitiu2 /> <br />
-      <Exercitiu3 /> <br />
-      <Exercitiu4 />
-      <br />
-    </div>
+    <Switch>
+      <Route path="/">
+        <div className="App">
+          <Header />
+          <Exercitiu1 />
+          <br />
+          <Exercitiu2 /> <br />
+          <Exercitiu3 /> <br />
+          <Exercitiu4 />
+          <br />
+        </div>
+      </Route>
+    </Switch>
   );
 }
 
